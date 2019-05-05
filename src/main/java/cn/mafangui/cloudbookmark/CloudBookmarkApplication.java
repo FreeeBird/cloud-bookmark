3f8ae9a39b8f2a141767aff947f1e5a86301a446
FreeeBird <1032796097@qq.com>
2019-05-05 22:17:31 +0800

    Initial commit

---
+++ src/main/java/cn/mafangui/cloudbookmark/CloudBookmarkApplication.java
@@ -0,0 +1,13 @@
+package cn.mafangui.cloudbookmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBookmarkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudBookmarkApplication.class, args);
+    }
+
+}
